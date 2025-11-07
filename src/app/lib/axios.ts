6678cb9565f3ea4cbcdf5017b2de7ba1195cc953
azizirfan-dev <azizirfan60@gmail.com>
2025-11-07 21:55:19 +0700

    fixed axios

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -4,7 +4,7 @@ const APP_ID = process.env.NEXT_PUBLIC_BACKENDLESS_APP_ID;
 const API_KEY = process.env.NEXT_PUBLIC_BACKENDLESS_API_KEY;
 
 const api = axios.create({
-  baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}`,
+  baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}/`,
   headers: { "Content-Type": "application/json" },
 });
 
@@ -21,11 +21,11 @@ api.interceptors.request.use((config) => {
 
 // 🟢 Bedakan antara API data & API user
 export const dataApi = axios.create({
-  baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}/data`,
+  baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}/data/`,
   headers: { "Content-Type": "application/json" },
 });
 
 export const userApi = axios.create({
-  baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}/users`,
+  baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}/users/`,
   headers: { "Content-Type": "application/json" },
 });
